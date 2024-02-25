c16bf699c3a1c018d7e4d95ea3b34e4d1338634f
Federico Airoldi <airoldibusiness@gmail.com>
2024-02-25 18:15:17 +0100

    create heading and quiz component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+
+import Logo from '../assets/quiz-logo.png';
+
+
+
+export default function Header() {
+
+	return (
+		<header>
+			<img src={Logo} alt="Logo"/>
+			<h1>React Quiz</h1>
+		</header>
+	);
+}
