c56513e38ddfc4dc0bef09e81f1617bdacbbeb16
Federico Airoldi <airoldibusiness@gmail.com>
2024-02-29 21:16:23 +0100

    summary component and other functions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -16,6 +16,16 @@ export default function Question({
 		isCorrect: null
 	});
 
+	let timer = 10000;
+
+	if (answer.selectedAnswers) {
+		timer = 1000;
+	}
+
+	if (answer.isCorrect !== null) {
+		timer = 2000;
+	}
+
 	function handleSelectAnswer(answer) {
 		setAnswer({
 			selectedAnswers: answer,
@@ -46,7 +56,12 @@ export default function Question({
 	return (
 
 		<div id="question">
-			<QuestionTimer timeout={10000} onTimeout={onSkipAnswer}/>
+			<QuestionTimer
+				key={timer}
+				timeout={timer}
+				onTimeout={answer.selectedAnswers === '' ? onSkipAnswer : null}
+				mode={answerState}
+			/>
 
 			<h2>{Questions[index].text}</h2>
 
